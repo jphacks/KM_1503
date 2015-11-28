c63dc921cf079f9bb9f09718d9bee83a695306dd
nagisa52 <may.2nd_@outlook.jp>
2015-11-27 23:59:55 -0500

    [server] space取得apiの実装

--- LimitedSpace/app/models/space.rb
+++ LimitedSpace/app/models/space.rb
@@ -1,3 +1,34 @@
 class Space < ActiveRecord::Base
   soft_deletable
+
+  def isExists( create_at , span )
+    til = created_at.to_i + span.to_i
+    if Time.now.to_i > til
+      exist = false
+    else
+      exist = true
+    render :json => exist
+  end
+
+  def isInRange( lat , lng )
+    til =  sqrt(( self.lat ** 2 ) + ( self.lng ** 2 ))
+    if sqrt(( lat ** 2 ) + ( lng ** 2 )) <= til
+
+      render :json => true
+    else
+      render :json => false
+  end
+
+  def getSpaces( lat, lng )
+    spaces = Spece.without_soft_destroyed
+    retspaces = Array.new
+    each do |spaces|
+      if ( Space.isExists && Space.isInRange )
+        retspacces << spaces
+      end
+    end
+    render :json => retspaces
+
+  end
+
 end
